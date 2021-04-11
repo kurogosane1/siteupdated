8b20bd089ab4bc890a3a0e0b1cb9b0f3a6369550
Syed Khurshid <magnite@hotmail.com>
2021-04-11 01:32:29 -0500

    got the card ready

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,28 @@
 import Head from "next/head";
 import styles from "../styles/About.module.css";
 
-
 const about = () => {
   return (
     <div className={styles.aboutContainer}>
       <Head>
         <title>About</title>
-        <meta name="About" content="A little fun fact about me." />
+        <meta name="title" content="A little fun fact about me." />
       </Head>
-      <h1 className="">About Me</h1>
-      <div className="">
-        <p>
-          A little fun fact about me. During the day I am a working Accountant
+      <h1 className={styles.aboutTitle}>About Me</h1>
+      <div className={styles.aboutContent}>
+        <span>
+          {/* A little fun fact about me. During the day I am a working Accountant
           for an Oil and Gas Company. During the evening I am a coder, artist
           and currently at the same time working towards a fit and healthy life.
           I really like to enjoy my life to the fullest, and love to learn new
-          things.
-        </p>
-        <p>
+          things. */}
+          A little fun fact about me. I am an Accountant. Who could have thought
+          that an accountant would actually also be a web developer. I always
+          knew that I had a deep passion for creating things but I didn't know
+          that it was in coding.
+        </span>
+        <br />
+        <span>
           I've always had a deep passion for Art. It was what made me think far
           and wide. A place where I could dream something and that would happen.
           I had been drawing since I could remember, and it was my way of being
@@ -26,8 +30,9 @@ const about = () => {
           Numbers surprisingly didn't mean anything to me. Infact, I remember I
           used to hate numbers a lot and was terrible in maths. I came accross
           accounting and learnt that even numbers have stories
-        </p>
-        <p>
+        </span>
+        <br />
+        <span>
           When I started working as a Accountant, I learnt that some of the
           tools we used to ease our work was made by accountants who knew how to
           code. That got me interested in coding. I sprung into action to find
@@ -35,24 +40,27 @@ const about = () => {
           This was the best decision I made as it is where I discovered that I
           could combine all three Art, Numbers and Code together to create
           something. I never was able to find that before.
-        </p>
-        <p>
+        </span>
+        <br />
+        <span>
           Today I'm a Certified Full Stack Developer, Accountant and Artist. I
           believe that soon one day I shall have the opportunity to combine all
-          of them together. Till them, I shall gladly participate in hackathons,
-          Art challenges, and marathons.
-        </p>
+          of them together. I am now working towards getting the opportunity to
+          become a developer and hope to join the developer side soon
+        </span>
       </div>
       <br />
-      <div className="Resume-section">
-        <button className="Resume" type="button">
-          <a
-            className="resume-link"
-            href="https://docs.google.com/document/d/1EpGDi3vhXRIigz3_bMSFTi1jfqDYGawJiYxAocgQ8Mw/edit?usp=sharing">
-            Resume
-          </a>
-        </button>
-      </div>
+      <button
+        className={styles.resume}
+        type="button"
+        onClick={() =>
+          window.open(
+            "https://docs.google.com/document/d/1EpGDi3vhXRIigz3_bMSFTi1jfqDYGawJiYxAocgQ8Mw/edit?usp=sharing",
+            "_blank"
+          )
+        }>
+        Resume
+      </button>
     </div>
   );
 };
--- pages/projects.js
+++ pages/projects.js
@@ -1,253 +1,86 @@
 import React, { useEffect } from "react";
+import styles from "../styles/Projects.module.css";
 import axios from "axios";
 
 const projects = (props) => {
-  useEffect(() => {
-    console.log(props);
-  }, []);
+  const data = [
+    {
+      heading: "Tiptuity",
+      cardImage: "https://i.imgur.com/fanvadO.png",
+      description:
+        "Tiptuity your cashless way of paying tips. Using your phone to scan the employee bar code and paying tips. Valet Employers using the tool can use track and monitor how much tip each employee has received and the vendors or location they were working receiving the tip. They add or remove employees, clients",
+      toolsUsed:
+        "ReactJS, NodeJS, ExpressJS, MySQL, ContextAPI,API,Axios,PassportJS, Material UI, MongoDB",
+      githubLink: "https://github.com/kurogosane1/tiptuity",
+      websiteLink: "https://tiptuityv2.herokuapp.com/",
+    },
+    {
+      heading: "Linux is Beautiful",
+      cardImage: "https://i.imgur.com/fanvadO.png",
+      description:
+        "A E-commerce website that was inspired Apple a online way of purchasing customized laptops related to Linux. Users are able to customize the Laptop and Tablet",
+      toolsUsed:
+        "ReactJS,NodeJS,ExpressJS,MySQL,ContextAPI,API,Axios,PassportJS, Material UI, MongoDB",
+      githubLink: "https://github.com/kurogosane1/Linux-is-beautifulv3.0",
+      websiteLink: "https://calm-dawn-32096.herokuapp.com",
+    },
+    {
+      heading: "Movie Database",
+      cardImage: "https://i.imgur.com/9fkTdxDl.png",
+      description:
+        "A React based rendition of the moviedatabase that has an API connection.",
+      githubLink: "https://github.com/kurogosane1/Movie-Database-App",
+      toolsUsed: "ReactJS,React-Spring,API,JSON,IMDB API",
+      websiteLink: "https://moviedb22222222.herokuapp.com/",
+    },
+    {
+      heading: "Markdown Electron NoteTaker",
+      cardImage:
+        "https://camo.githubusercontent.com/763ef9b670dec1766f11d76af9dfb0b57037b129/68747470733a2f2f692e696d6775722e636f6d2f314e795679556d2e706e67",
+      description:
+        "A ReactJS mixed with ElectronJS combined to produce a Markdown Notetaking app. The purpose is simply to show that ReactJS is capable of working with Electron. Meant to be run as a Desktop Application only!",
+      toolsUsed: "ReactJS,ElectronJS,API,NodeJS, ExpressJS",
+      githubLink: "https://github.com/kurogosane1/ElectronNoteTaking",
+      websiteLink: "",
+    },
+  ];
 
   return (
-    <div>
-      <div id="Port-heading">
-        <h2>Personal Projects and Work</h2>
+    <div className={styles.projectContainer}>
+      <div className={styles.projectsHeader}>
+        <h2>Projects</h2>
       </div>
-      <div className="container">
-        <div className="card">
-          <div className="card-header">
-            <h2>Movie Database</h2>
+      <div className={styles.projectsContainer}>
+        <div className={styles.card}>
+          <div className={styles.information}>
+            <h2>Sample Test</h2>
+            <div className={styles.description}>
+              <span>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+                tempora esse maxime omnis, ab, nisi soluta rem incidunt
+                voluptatibus recusandae quidem unde officia? Commodi quaerat
+                dicta laudantium odit, beatae sint.
+              </span>
+            </div>
+            <div>
+              <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                minus voluptatum, nemo, reiciendis excepturi ratione quisquam
+                enim inventore delectus veniam maiores quae, possimus quis
+                facere assumenda cumque consectetur consequuntur. Sequi!
+              </span>
+            </div>
           </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/9fkTdxDl.png" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>
-              A React based rendition of the moviedatabase that has an API
-              connection.
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/kurogosane1/Movie-Database-App"
-            />
-            <a
-              className="fas fa-globe fa-3x"
-              href="https://moviedb22222222.herokuapp.com/"
-            />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>Markdown Electron NoteTaker</h2>
-          </div>
-          <div className="card-img">
+          <div className={styles.images}>
             <img
-              src="https://camo.githubusercontent.com/763ef9b670dec1766f11d76af9dfb0b57037b129/68747470733a2f2f692e696d6775722e636f6d2f314e795679556d2e706e67"
-              alt=""
-            />
-          </div>
-          <div className="card-detail">
-            <span>
-              A ReactJS mixed with ElectronJS combined to produce a Markdown
-              Notetaking app. The purpose is simply to show that ReactJS is
-              capable of working with Electron. Meant to be run as a Desktop
-              Application only!
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/kurogosane1/ElectronNoteTaking"
-            />
-            <a className="fas fa-globe fa-3x" />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>TipTuity</h2>
-          </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/fanvadO.png" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>
-              Tiptuity your cashless way of paying tips. Using your phone to
-              scan the employee bar code and paying tips. Made with SQL and
-              MongoDB with HandleBarsJS and NodeJS as the front and back
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/kurogosane1/tiptuity"
-            />
-            <a
-              className="fas fa-globe fa-3x"
-              href="https://tiptuity.herokuapp.com"
+              src="https://static.toiimg.com/photo/75503656.cms"
+              alt="sample-example"
             />
           </div>
         </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>Eat-Da-Burger</h2>
-          </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/iXrMKVam.png" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>
-              Eat-Da-Burger! is a restaurant app that lets users input the names
-              of burgers they'd like to eat.A Single-Page-Light App. Work with
-              both front and back end and database (MySQL) allows users to put a
-              name of burger and eat it. Rendering the page with handlebars and
-              relate data with ORM technique
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/kurogosane1/sequelizedDatBurger"
-            />
-            <a
-              className="fas fa-globe fa-3x"
-              href="https://tranquil-brook-67807.herokuapp.com/"
-            />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>Trivia Game</h2>
-          </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/v3grL3g.png" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>
-              TriviaGame A game that questions your knowledge on Disney. You
-              shall be timed as you answer the questions. If your time runs out.
-              You shall lose your ability to answer any questions and therefore
-              will no longer be able to play the game.
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/kurogosane1/TriviaGame"
-            />
-            <a
-              className="fas fa-globe fa-3x"
-              href="https://pure-gorge-43433.herokuapp.com"
-            />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>HackCity</h2>
-          </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/GA4A9hpm.png" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>
-              HackCity React where Web Developers, Desiginers get together,
-              collaborate, work together and create things together.This was
-              made with HTML5, CSS3, NodeJS and MySQL that deals with the
-              BackEnd side of things.A collaborative work together with other
-              members
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/mrkevinsmithdesign/hackCityTwo"
-            />
-            <a
-              className="fas fa-globe fa-3x"
-              href="https://stark-ravine-93008.herokuapp.com/"
-            />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>NYTScrapper</h2>
-          </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/2xWUYXU.png" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>
-              A react-based rendition of the New York Times Article Search
-              Application This utilize the NYT API to search up articles by
-              terms and years (optional). Then you can save the article to a
-              NoSQL database so you can read the article in the future. There is
-              a tab to display all the saved articles and you can delete any of
-              the articles, if you so do desire. strong This is all running on
-              react strong
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/kurogosane1/NYTReactSearch"
-            />
-            <a
-              className="fas fa-globe fa-3x"
-              href="https://agile-coast-87413.herokuapp.com/"
-            />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>Angular Profile</h2>
-          </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/IEcdQ7fl.png" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>
-              A website that I re-imagined to be the version of my website. I
-              imagined using Angular and it was more of a personal test to see
-              my website work
-            </span>
-          </div>
-          <div className="card-social">
-            <a
-              className="fab fa-github fa-3x"
-              href="https://github.com/kurogosane1/AngularProfile"
-            />
-            <a
-              className="fas fa-globe fa-3x"
-              href="https://shrouded-thicket-80653.herokuapp.com/"
-            />
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-header">
-            <h2>Coming Soon</h2>
-          </div>
-          <div className="card-img">
-            <img src="https://i.imgur.com/BuAkZKU.jpg" alt="" />
-          </div>
-          <div className="card-detail">
-            <span>More is Coming Soon!!!!!!</span>
-          </div>
-          <div className="card-social" />
-        </div>
-        <br />
       </div>
     </div>
   );
 };
 
 export default projects;
-
-export async function getServerSideProps(context) {
-  const data = await axios
-    .get("/api/data")
-    .then((response) => response)
-    .catch((err) => console.log(err));
-  console.log(`this is line 10` + data);
-  return {
-    props: { da: '' },
-  };
-}
